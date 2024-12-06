c03f9f2618f201d855b6adc450eda6d82914af48
t22cs050 <t22cs050@gmail.com>
2024-12-06 16:27:28 +0900

    IDとコード生成を実装中

--- careLink/models.py
+++ careLink/models.py
@@ -3,15 +3,28 @@ from django.db import models
 
 class Elder(models.Model):
     # 5桁以上10桁未満の整数
-    elder_id = models.IntegerField()
+    elder_id = models.IntegerField(unique=True)
     # 4桁の整数
-    elder_code = models.IntegerField()
+    elder_code = models.IntegerField(unique=True)
+
+    def save(self, *args, **kwargs):
+        # elder_idを自動生成（5桁以上10桁未満）
+        if not self.elder_id:
+            self.elder_id = generate_unique_integer(
+                Elder, 'elder_id', 10000, 999999999
+            )
+        # elder_codeを自動生成（4桁の整数）
+        if not self.elder_code:
+            self.elder_code = generate_unique_integer(
+                Elder, 'elder_code', 1000, 9999
+            )
+        super().save(*args, **kwargs)  # 元のsaveメソッドを呼び出す
 
 
 class Family(models.Model):
     # 5桁以上10桁未満の整数
-    family_id = models.IntegerField()
+    family_id = models.IntegerField(unique=True)
     # 4桁の整数
-    elder_code = models.IntegerField()
+    elder_code = models.IntegerField(unique=True)
     name = models.CharField(max_length=30)
     password = models.CharField(max_length=20)
--- careLink/views.py
+++ careLink/views.py
@@ -4,6 +4,7 @@ from .models import Elder, Family
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import get_object_or_404
+from .randomGenerate import generate_unique_integer
 
 
 def login(request):
@@ -15,6 +16,10 @@ class signInElder(CreateView):
     fields = ()
     template_name = 'careLink/elder_add.html'
     success_url = '/careLink/login'
+    
+    def form_valid(self, form):
+        # form.save()の前にidとcodeが生成されるようにする
+        return super().form_valid(form)
 
 
 class signInFamily(CreateView):
