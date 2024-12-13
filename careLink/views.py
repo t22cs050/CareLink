b4916765fd9c60f078b4ca2f948059d1443fec80
t22cs050 <t22cs050@gmail.com>
2024-12-13 16:06:33 +0900

    [fix]マージ後の競合の修正

--- careLink/views.py
+++ careLink/views.py
@@ -48,37 +48,39 @@ class signUpFamily(CreateView):
     template_name = 'careLink/family_add.html'
     success_url = '/careLink/login'
 
-    # 行動状況の確認
-    def result(request):
-        # クエリパラメータから日付情報を取得
-        date_str = request.GET.get('date')
-        selected_date = None
 
-        if date_str:
-            try:
-                # 日付情報をパース
-                selected_date = datetime.strptime(date_str, '%Y-%m-%d').date()
-            except ValueError:
-                selected_date = None  # パースエラー時はNoneに設定
+# 行動状況の確認
+def result(request):
+    # クエリパラメータから日付情報を取得
+    date_str = request.GET.get('date')
+    selected_date = None
 
-        # 現在の月を取得
-        month_current = datetime.now()
+    if date_str:
+        try:
+            # 日付情報をパース
+            selected_date = datetime.strptime(date_str, '%Y-%m-%d').date()
+        except ValueError:
+            selected_date = None  # パースエラー時はNoneに設定
 
-        # テンプレートにデータを渡す
-        return render(request, 'careLink/result.html', {
-            'selected_date': selected_date,
-            'month_current': month_current,
-        })
+    # 現在の月を取得
+    month_current = datetime.now()
 
-    # --- 月間カレンダーを表示するビュー ---
-    class MonthCalendar(mixins.MonthCalendarMixin, TemplateView):
-        
-        template_name = 'careLink/result-calender.html'
+    # テンプレートにデータを渡す
+    return render(request, 'careLink/result.html', {
+        'selected_date': selected_date,
+        'month_current': month_current,
+    })
 
-        def get_context_data(self, **kwargs):
-            context = super().get_context_data(**kwargs)
-            calendar_context = self.get_month_calendar()
-            context.update(calendar_context)
-            return context
+
+# --- 月間カレンダーを表示するビュー ---
+class MonthCalendar(mixins.MonthCalendarMixin, TemplateView):
+    
+    template_name = 'careLink/result-calender.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        calendar_context = self.get_month_calendar()
+        context.update(calendar_context)
+        return context
 
 
